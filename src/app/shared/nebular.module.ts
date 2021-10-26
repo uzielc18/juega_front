d6462ce89d64b76e3f872c256cf3ee57a57ba100
Yobel Tayson <rotkyobel@gmail.com>
2021-10-26 15:16:44 -0500

    ✨ edit button added

--- src/app/shared/nebular.module.ts
+++ src/app/shared/nebular.module.ts
@@ -1,64 +1,68 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import {
-    NbAccordionModule, NbActionsModule,
-    NbAlertModule,
-    NbAutocompleteModule,
-    NbButtonModule,
-    NbCardModule,
-    NbCheckboxModule, NbContextMenuModule,
-    NbDatepickerModule,
-    NbDialogModule,
-    NbFormFieldModule,
-    NbIconModule,
-    NbInputModule, NbLayoutModule, NbListModule, NbMenuModule, NbPopoverModule,
-    NbProgressBarModule,
-    NbRadioModule,
-    NbSelectModule, NbSidebarModule, NbSpinnerModule,
-    NbTabsetModule,
-    NbTimepickerModule, NbToggleModule,
-    NbTooltipModule
+  NbAccordionModule,
+  NbActionsModule,
+  NbAlertModule,
+  NbAutocompleteModule,
+  NbButtonGroupModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbContextMenuModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbFormFieldModule,
+  NbIconModule,
+  NbInputModule,
+  NbListModule,
+  NbMenuModule,
+  NbPopoverModule,
+  NbProgressBarModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbSpinnerModule,
+  NbTabsetModule,
+  NbTimepickerModule,
+  NbToggleModule,
+  NbTooltipModule,
 } from '@nebular/theme';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { DialogConfimComponent } from './components/dialog-confim/dialog-confim.component';
 
-const ANGULAR: any[] = [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-];
+const ANGULAR: any[] = [CommonModule, FormsModule, ReactiveFormsModule];
 
 const COMPONENTS: any[] = [
-    NbCardModule,
-    NbTabsetModule,
-    NbButtonModule,
-    NbIconModule,
-    NbInputModule,
-    NbSelectModule,
-    NbAutocompleteModule,
-    NbDatepickerModule,
-    NbTimepickerModule,
-    NbRadioModule,
-    NbCheckboxModule,
-    NbTooltipModule,
-    NbToggleModule,
-    NbDialogModule.forChild(),
-    NbAlertModule,
-    NbListModule,
-    NbAccordionModule,
-    NbActionsModule,
-    NbPopoverModule,
-    NbSpinnerModule,
-    NbContextMenuModule,
-    NbProgressBarModule,
-    NbFormFieldModule,
-    NbMenuModule
+  NbCardModule,
+  NbTabsetModule,
+  NbButtonModule,
+  NbIconModule,
+  NbInputModule,
+  NbSelectModule,
+  NbAutocompleteModule,
+  NbDatepickerModule,
+  NbTimepickerModule,
+  NbRadioModule,
+  NbCheckboxModule,
+  NbTooltipModule,
+  NbToggleModule,
+  NbDialogModule.forChild(),
+  NbAlertModule,
+  NbListModule,
+  NbAccordionModule,
+  NbActionsModule,
+  NbPopoverModule,
+  NbSpinnerModule,
+  NbContextMenuModule,
+  NbProgressBarModule,
+  NbFormFieldModule,
+  NbMenuModule,
+  NbButtonGroupModule,
 ];
 
 @NgModule({
-    declarations: [DialogConfimComponent],
-    imports: [...ANGULAR, ...COMPONENTS],
-    exports: [...ANGULAR, ...COMPONENTS]
+  declarations: [DialogConfimComponent],
+  imports: [...ANGULAR, ...COMPONENTS],
+  exports: [...ANGULAR, ...COMPONENTS],
 })
-export class NebularModule {
-}
+export class NebularModule {}
