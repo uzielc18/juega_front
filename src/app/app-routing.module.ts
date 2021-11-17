1b69443b8ddd07b9a145da8acae250e03b7ffb51
Yobel <rotkyobel@gmail.com>
2021-11-17 08:57:46 -0500

    ✨ (add): homework form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     component: ScaffoldComponent,
     data: {
       breadcrumb: {
-        label: 'Home',
+        label: 'Inicio',
       }
     },
     canActivate: [Auth2Guard],
