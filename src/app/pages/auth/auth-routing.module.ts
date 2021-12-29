711e7c2af2e92c29a11e81a6b8d49ca5ad606fe3
Yobel <rotkyobel@gmail.com>
2021-12-29 08:46:04 -0500

    logout working

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IsLoggedGuard } from '../../core/oauth2/is-logged.guard';
 import { LoginComponent } from './contents/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    canActivate: [IsLoggedGuard],
     children: [
       {
         path: 'login',
