7f26bbcb9f7dc410537d5d0c520f735e7131185e
unknown <armando_huarcaya@upeu.edu.pe>
2022-01-11 16:38:15 -0500

    changes config enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,20 +5,19 @@ export const environment = {
       name: 'lamb',
       clientId: 'CCoDFWUHL6OJ4sus9eEIUh6Vk62lNCAnPGY3FXgg',
       baseEndpoint: 'https://oauth.upeu.edu.pe',
-      redirectUri: `https://upeu.edu.patmos.pe/patmos-upeu-base-front/oauth2/callback`,
-      success: '/patmos-upeu-base-front/pages/dashboard',
+      redirectUri: `https://upeu.patmos.edu.pe/oauth2/callback`,
+      success: '/pages/dashboard',
     },
     authGoogleStrategy: {
       name: 'google',
-      clientId:
-        '649461344129-0thst4vl3n9aj0j7njie8nd73psls419.apps.googleusercontent.com',
+      clientId:'649461344129-0thst4vl3n9aj0j7njie8nd73psls419.apps.googleusercontent.com',
       endpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-      redirectUri: `https://upeu.edu.patmos.pe/patmos-upeu-base-front/oauth2/google/callback`,
-      success: '/patmos-upeu-base-front/pages/dashboard',
+      redirectUri: `https://upeu.patmos.edu.pe/oauth2/google/callback`,
+      success: '/pages/dashboard',
     },
     apiUrls: {
-        auth: 'https://api.patmos.upeu.edu.pe/',
-        base: 'https://api.patmos.upeu.edu.pe/',
+        auth: 'https://api.patmos.edu.pe/',
+        base: 'https://api.patmos.edu.pe/',
     },
     shellApp: `${window.location.origin}`,
 };
