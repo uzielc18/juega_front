29abd416aa67dfe8a9e0aadd741e58230ac92568
unknown <armando_huarcaya@upeu.edu.pe>
2022-01-11 18:28:35 -0500

    changes

--- src/app/providers/utils/end-points.ts
+++ src/app/providers/utils/end-points.ts
@@ -6,7 +6,7 @@ export const END_POINTS = {
   base_back: { // Proyecto
     resourse: 'resources-person',
     elements: 'elements',
-    user: '/user',
+    user: 'user',
     config: 'config',
   },
 };
