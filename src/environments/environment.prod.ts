4bf8a6e70b5d4319477c8b67c376212427aa7926
Edgar Raul Hugo <edgar.hugo@lim.upeu.edu.pe>
2022-01-12 18:34:55 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,12 +12,12 @@ export const environment = {
       name: 'google',
       clientId:'649461344129-0thst4vl3n9aj0j7njie8nd73psls419.apps.googleusercontent.com',
       endpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-      redirectUri: `https://upeu.patmos.edu.pe/oauth2/google/callback`,
+      redirectUri: `https://lamb-elearning.upeu.edu.pe/oauth2/google/callback`,
       success: '/pages/dashboard',
     },
     apiUrls: {
-        auth: 'https://api.patmos.edu.pe',
-        base: 'https://api.patmos.edu.pe',
+        auth: 'https://api-lamb-elearning.upeu.edu.pe',
+        base: 'https://api-lamb-elearning.upeu.edu.pe',
     },
-    shellApp: `https://upeu.patmos.edu.pe/auth`,
+    shellApp: `https:/lamb-elearning.upeu.edu.pe/auth`,
 };
