ea1c2e986fc8a36732d1bffc86a7f708738f1c64
Yobel <rotkyobel@gmail.com>
2022-02-04 12:01:12 -0500

    changes

--- src/app/shared/components/menu-elements-child/menu-elements-child.component.ts
+++ src/app/shared/components/menu-elements-child/menu-elements-child.component.ts
@@ -19,21 +19,21 @@ export class MenuElementsChildComponent implements OnInit {
 
   @ViewChild('stickyMenu') menuElement!: ElementRef;
 
-  sticky: boolean = false;
-  elementPosition: any;
-  ngAfterViewInit(){
-    this.elementPosition = this.menuElement.nativeElement.offsetTop;
-  }
+  // sticky: boolean = false;
+  // elementPosition: any;
+  // ngAfterViewInit(){
+  //   this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  // }
 
-  @HostListener('window:scroll', ['$event'])
-    handleScroll(){
-      const windowScroll = window.pageYOffset;
-      if(windowScroll >= this.elementPosition){
-        this.sticky = true;
-      } else {
-        this.sticky = false;
-      }
-    }
+  // @HostListener('window:scroll', ['$event'])
+  //   handleScroll(){
+  //     const windowScroll = window.pageYOffset;
+  //     if(windowScroll >= this.elementPosition){
+  //       this.sticky = true;
+  //     } else {
+  //       this.sticky = false;
+  //     }
+  //   }
 
   constructor(private generalServi: GeneralService) { }
 
