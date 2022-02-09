8829608f9a00edbbed57089b6efc7f1f5f57bea4
Yobel <rotkyobel@gmail.com>
2022-02-09 10:16:24 -0500

    update

--- src/app/shared/components/questions/q-config/c-relation/c-relation.component.ts
+++ src/app/shared/components/questions/q-config/c-relation/c-relation.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { GeneralService } from 'src/app/providers';
 import { DIRECTORY } from 'src/app/shared/directorios/directory';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-c-relation',
@@ -8,15 +9,13 @@ import { DIRECTORY } from 'src/app/shared/directorios/directory';
   styleUrls: ['./c-relation.component.scss']
 })
 export class CRelationComponent implements OnInit {
-  @Input() item: any;
+  @Input() headParams: any;
+  @Output() loadings: EventEmitter<boolean> = new EventEmitter();
   directorio: any = DIRECTORY.base;
-  arrayFile: any = [];
-  formHeader: any = FormGroup;
-  formHeaderFile: any = FormGroup;
   key_file: any;
 
   abecedario: any = [
-         "a", "b", "c", "d",
+    "a", "b", "c", "d",
     "e", "f", "g", "h", "i",
     "j", "k", "l", "m", "n",
     "o", "p", "q", "r", "s",
@@ -25,55 +24,136 @@ export class CRelationComponent implements OnInit {
   ];
 
   secondList: any[] = [];
-  relationList: any[] = [
+  relationList: any = [
     {
-      relacion: 'Lima',
-      relacionImg: 'lima.jpg',
-      resp: 'Peru',
-      respImg: 'peru.jpg',
-      puntos: 2,
-      correcto: 0
+      id: 0,
+      question_id: 0,
+      relacion: '',
+      imagen: '',
+      base64: '',
+      resp: '',
+      resp_img: '',
+      resp_imagen_base64: '',
+      puntos: 0,
+      correcto: 0,
+      orden: 1,
+      estado: 1,
     },
-    {
-      relacion: 'Quito',
-      relacionImg: 'quito.jpg',
-      resp: 'Ecuador',
-      respImg: 'ecuador.jpg',
-      puntos: 3,
-      correcto: 0
-    },
-    {
-      relacion: 'Montevideo',
-      relacionImg: 'montevideo.jpg',
-      resp: 'Uruguay',
-      respImg: 'uruguay.jpg',
-      puntos: 3,
-      correcto: 0
-    }
   ]
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
-    this.fieldReactiveFile();
+  constructor(private generalServi: GeneralService) { }
+  ngOnChanges(): void {
+    this.headParams = this.headParams;
   }
-
-  private fieldReactiveFile() {
-    const controls = {
-      file: ['', [Validators.required]],
-    };
-    this.formHeaderFile = this.formBuilder.group(controls);
-    this.key_file = this.item?.id_carga_curso_docente + '_' + Math.floor(Math.random() * 90000) + 10000;
+  ngOnInit(): void {
+    this.valueKey();
+  }
+  valueKey() {
+    this.key_file = 'rrrr' + '_' + '00000000001' + '_' + Math.floor(Math.random() * 90000) + 10000;
   }
 
   valueFile($event: any, item: any) {
     console.log($event);
-    item.claveImg = $event.value.nombre_s3;
+    item.imagen = $event.value.nombre_s3;
+    item.base64 = $event.value.base64;
   }
 
-  get validArray() {
+  valueFileResp($event: any, item: any) {
+    console.log($event);
+    item.resp_imagen = $event.value.nombre_s3;
+    item.resp_imagen_base64 = $event.value.base64;
+  }
+
+  pushedObject() {
+    const option = {
+      id: 0,
+      question_id: 0,
+      relacion: '',
+      imagen: '',
+      base64: '',
+      resp: '',
+      resp_imagen: '',
+      resp_imagen_base64: '',
+      puntos: 0,
+      correcto: 0,
+      orden: 1,
+      estado: 1,
+    }
+    this.relationList.push(option);
+  }
+  saveQuestion() {
+
+    this.secondList = JSON.parse(JSON.stringify(this.relationList));
+    this.secondList.forEach(object => {
+      object.relacion = object.resp;
+      object.imagen = object.resp_imagen;
+      delete object['resp'];
+      delete object['resp_imagen'];
+    });
+
+    // const array = this.relationList.filter((re: any) => re.checked === true);
+    // if (array.length > 0) {
+    //   this.relationList.map((r: any) => {
+    //     if (r.checked === true) {
+    //       r.correcto = 1;
+    //     } else {
+    //       r.correcto = 0;
+    //     }
+    //   });
+    //   const serviceName = '';
+    //   const params: any = {
+    //     section_id: 0,
+    //     type_alternative_id: 0,
+    //     exam_id: 0,
+    //     question_id: 0,
+    //     pregunta: this.headParams.pregunta,
+    //     help: '',
+    //     orden: this.headParams.orden || '',
+    //     url_video: this.headParams.url_video || '',
+    //     key_video: this.headParams.key_video || '',
+    //     adjunto: this.headParams.adjunto || '',
+    //     estado: this.headParams.estado,
+    //     codigo: '01',
+    //     nombre: 'Relacionar',
+    //     component: 'multiple-choice',
+    //     componentEdit: false,
+    //     alternatives: this.relationList || [],
+    //   };
+    //   if (params && params.pregunta && params.alternatives.length > 0) {
+    //     this.loadings.emit(true);
+    //     this.generalServi.addNameData$(serviceName, params).subscribe(r => {
+    //       if (r.success) {
+    //       }
+    //     }, () => { this.loadings.emit(true); }, () => { this.loadings.emit(true); });
+    //   }
+    // } else {
+      // Swal.fire({
+      //   title: 'Recuerda',
+      //   text: 'Debe relacionar',
+      //   backdrop: true,
+      //   icon: 'question',
+      //   // animation: true,
+      //   showCloseButton: true,
+      //   showCancelButton: true,
+      //   showConfirmButton: true,
+      //   confirmButtonColor: '#7f264a',
+      //   confirmButtonText: 'Ok',
+      //   // timer: 2000,
+      // });
+    // }
+  }
+  deleteItemFile(item: any) {
+    item.base64 = '';
+  }
+  deleteSecondItemFIle(itema: any) {
+    itema.resp_imagen_base64 = '';
+  }
+  deleteOption(i: any) {
+    this.relationList.splice(i, 1);
+  }
+  get validButom() {
     if (this.relationList.length > 0) {
-      const array = this.relationList.filter(r => !r.relacion || !r.resp || r.puntos < 0 || r.puntos === null);
+      const array = this.relationList.filter((r: any) => !r.relacion || !r.resp || r.puntos < 0 || r.puntos === null);
       if (array.length > 0) {
         return true;
       } else {
@@ -83,38 +163,7 @@ export class CRelationComponent implements OnInit {
       return true;
     }
   }
-
-  addRelation() {
-    const option = {
-      relacion: '',
-      relacionImg: '',
-      resp: '',
-      respImg: '',
-      puntos: 0,
-      correcto: 0
-    }
-    this.relationList.push(option);
-  }
-
   cancel() {
     this.relationList = [];
-  }
-
-  saveRelation() {
-    console.log(this.relationList);
-    this.secondList = JSON.parse(JSON.stringify(this.relationList));
-    this.secondList.forEach(object => {
-      object.relacion = object.resp;
-      object.relacionImg = object.respImg;
-      delete object['resp'];
-      delete object['respImg'];
-    });
-    console.log(this.secondList);
-  }
-
-  deleteRelation(i: any) {
-    this.relationList.splice(i, 1);
-    console.log(i);
-    console.log(this.relationList, 'nuevo');
   }
 }
