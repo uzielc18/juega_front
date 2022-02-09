7505b833c0d6067cb4106f8cda9db78b1d4379e6
Cristian Huarcaya <armando_huarcaya@upeu.edu.pe>
2022-02-09 12:03:13 -0500

    routing

---
+++ src/app/shared/components/questions/q-view/v-docente/v-doc-unique-option/v-doc-unique-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-v-doc-unique-option',
+  templateUrl: './v-doc-unique-option.component.html',
+  styleUrls: ['./v-doc-unique-option.component.scss']
+})
+export class VDocUniqueOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
