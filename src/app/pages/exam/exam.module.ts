b323b9392b275629d5ea9ee8a2cc68304d73be0c
Yobel <rotkyobel@gmail.com>
2022-02-10 09:35:25 -0500

    cambios

--- src/app/pages/exam/exam.module.ts
+++ src/app/pages/exam/exam.module.ts
@@ -5,17 +5,34 @@ import { ExamRoutingModule } from './exam-routing.module';
 import { ExamHomeComponent } from './containers/exam-home.component';
 import { ExamComponent } from './exam.component';
 import { NebularModule } from '../../shared/nebular.module';
+import { VEstRelationComponent } from './components/v-est-relation/v-est-relation.component';
+import { VEstMultiOptionComponent } from './components/v-est-multi-option/v-est-multi-option.component';
+import { VEstUniqueOptionComponent } from './components/v-est-unique-option/v-est-unique-option.component';
 
+const COMPONENTS: any = [
+  ExamHomeComponent,
+  ExamComponent,
+  VEstRelationComponent,
+  VEstMultiOptionComponent,
+  VEstUniqueOptionComponent
+]
+
+const MODULES: any = [
+
+]
+
+const NEBULAR: any = [
+  NebularModule
+]
 
 @NgModule({
   declarations: [
-    ExamHomeComponent,
-    ExamComponent
+    ...COMPONENTS
   ],
   imports: [
     CommonModule,
     ExamRoutingModule,
-    NebularModule
+    ...NEBULAR
   ]
 })
 export class ExamModule { }
