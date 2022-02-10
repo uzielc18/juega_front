12d1b5a324e859e6bcc49b5194c69916f39e3351
Cristian Huarcaya <armando_huarcaya@upeu.edu.pe>
2022-02-10 15:19:16 -0500

    Evaluación docente

--- src/app/pages/asignatures/components/views/v-course/v-course.component.ts
+++ src/app/pages/asignatures/components/views/v-course/v-course.component.ts
@@ -17,21 +17,6 @@ export class VCourseComponent implements OnInit, OnDestroy {
   path: number = 0;
   curso: any = [];
   loading:boolean = false;
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
   constructor(
     private activatedRoute: ActivatedRoute,
     private generalService: GeneralService,
