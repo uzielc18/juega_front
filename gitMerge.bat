0f395d9c120aab8c6c1ba32fbf2a12c893b3c3f6
Yobel <rotkyobel@gmail.com>
2022-02-13 22:24:53 -0500

    13/02/2022 22:24:47 -> rest

--- gitMerge.bat
+++ gitMerge.bat
@@ -1,11 +1,18 @@
 @echo off
 
-set date = [System.DateTime]::Now.ToString("yyyy-MM-dd-HH-mm-ss.fff")
+for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
+
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+set hour=%datetime:~8,2%
+set minute=%datetime:~10,2%
+set second=%datetime:~12,2%
 
 git status
 git add .
 git status
 set /p message="Ingresa el mensaje del commit: "
-git commit -m "%date% -> %message%"
+git commit -m "%day%/%month%/%year% %hour%:%minute%:%second% -> %message%"
 git pull
 git pull origin develop
