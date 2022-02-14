14b7a4a1c9d1b71af3c77fa877ca63c0fd4a4956
Yobel <rotkyobel@gmail.com>
2022-02-13 21:40:02 -0500

    02/13/22 -> dddd

--- gitMerge.bat
+++ gitMerge.bat
@@ -1,9 +1,11 @@
 @echo off
 
+set date = [System.DateTime]::Now.ToString("yyyy-MM-dd-HH-mm-ss.fff")
+
 git status
 git add .
 git status
 set /p message="Ingresa el mensaje del commit: "
-git commit -m "%message%"
+git commit -m "%date% -> %message%"
 git pull
 git pull origin develop
