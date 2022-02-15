4f0c0a70cc080ac06a271362f3f5c4175c870bc4
Edwin Calsin Quinto <edwin_calsin@upeu.edu.pe>
2022-02-15 16:49:39 -0500

    actualizando cerrar sesión

--- src/app/core/state/app-validate-token.service.ts
+++ src/app/core/state/app-validate-token.service.ts
@@ -1,18 +1,26 @@
-import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CORE_OPTIONS, CoreOptions } from '../core.options';
-import { DOCUMENT } from '@angular/common';
-import { environment } from 'src/environments/environment';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import {Inject, Injectable} from '@angular/core';
+import {HttpBackend, HttpClient, HttpHeaders} from '@angular/common/http';
+import {CORE_OPTIONS, CoreOptions} from '../core.options';
+import {DOCUMENT} from '@angular/common';
+import {environment} from 'src/environments/environment';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {AppService} from "./app.service";
 
 @Injectable()
 export class AppValidateTokenService {
+
+  private httpClientForLogout: HttpClient;
+
   constructor(
+    private appService: AppService,
+    private handler: HttpBackend,
     protected httpClient: HttpClient,
     @Inject(CORE_OPTIONS) protected options: CoreOptions,
     @Inject(DOCUMENT) protected document: any
-  ) {}
+  ) {
+    this.httpClientForLogout = new HttpClient(handler);
+  }
 
   validateLamb(access_token: any): Observable<any> {
     return this.httpClient.post<any>(`${this.options.apiAuth}/api/loginLamb`, {
@@ -29,6 +37,13 @@ export class AppValidateTokenService {
     );
   }
 
+
+  logout() {
+    const token = this.appService.user.hasOwnProperty('access_token') ? this.appService.user.access_token : '';
+    return this.httpClientForLogout.post(`${environment.authStrategy.baseEndpoint}/api/logout_all/`, {}, {headers: new HttpHeaders({'Authorization': `Bearer ${token}`})})
+  };
+
+
   // Change
   // validate(token: any): Promise<any> {
   // return this.httpClient.post(`${this.options.apiAuth}/api/oauth/valid-tokens-oauth`, {token}).toPromise();
@@ -41,25 +56,25 @@ export class AppValidateTokenService {
   //   this.document.location.href = `${environment.authStrategy.baseEndpoint}/accounts/logout?${next}${encodeParamRequest}`;
   // }
 
-    authorizeLamb(): void {
-      const paramRequest = AppValidateTokenService.paramsLamb();
-      const next = 'next=/oauth/authorize/';
-      const encodeParamRequest = encodeURIComponent(`${paramRequest}`);
-      this.document.location.href = `${environment.authStrategy.baseEndpoint}/accounts/logout?${next}${encodeParamRequest}`;
-    }
+  authorizeLamb(): void {
+    const paramRequest = AppValidateTokenService.paramsLamb();
+    const next = 'next=/oauth/authorize/';
+    const encodeParamRequest = encodeURIComponent(`${paramRequest}`);
+    this.document.location.href = `${environment.authStrategy.baseEndpoint}/accounts/logout?${next}${encodeParamRequest}`;
+  }
 
-    authorizeGoogle(): void {
-      const paramRequest = AppValidateTokenService.paramsGoogle();
-      const next = 'next=/oauth/authorize/';
-      const encodeParamRequest = encodeURIComponent(`${paramRequest}`);
-      this.document.location.href = `${environment.authGoogleStrategy.endpoint}/accounts/logout?${next}${encodeParamRequest}`;
-    }
+  authorizeGoogle(): void {
+    const paramRequest = AppValidateTokenService.paramsGoogle();
+    const next = 'next=/oauth/authorize/';
+    const encodeParamRequest = encodeURIComponent(`${paramRequest}`);
+    this.document.location.href = `${environment.authGoogleStrategy.endpoint}/accounts/logout?${next}${encodeParamRequest}`;
+  }
 
-    // authorize(): void {
-    //   const next = 'next=' +  '/oauth/authorize/';
-    //   this.document.location.href = `${environment.authStrategy.baseEndpoint}/accounts/logout?${next}`;
-    //   // window.location.href = environment.shellApp;
-    // }
+  // authorize(): void {
+  //   const next = 'next=' +  '/oauth/authorize/';
+  //   this.document.location.href = `${environment.authStrategy.baseEndpoint}/accounts/logout?${next}`;
+  //   // window.location.href = environment.shellApp;
+  // }
   //   authorize(): void {
   //     const paramRequest = AppValidateTokenService.params();
   //     const next = 'next=/oauth/authorize/';
