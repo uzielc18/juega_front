7df12efc41a7b3c4bc94aa9375d25cd1e717c381
Yobel <rotkyobel@pm.me>
2022-05-26 17:22:45 -0500

    26/05/2022 17:22:39 -> fyfs

---
+++ src/app/pages/element/containers/element-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-element-home',
+  templateUrl: './element-home.component.html',
+  styleUrls: ['./element-home.component.scss']
+})
+export class ElementHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
