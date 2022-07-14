8c600e88fe9f640b371fbbbec2eab4fce907cadc
Ever Saul <ever.hilasaca@cloudcomputing.com.pe>
2022-07-14 13:27:01 -0500

    14/07/2022 13:26:52 -> cambios diseño

--- src/app/pages/films/containers/films-home.component.ts
+++ src/app/pages/films/containers/films-home.component.ts
@@ -3,6 +3,9 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 import {MUnitSessionComponent} from "../../../shared/components/unit-session/modal/m-unit-session.component";
 import {MAddSessionComponent} from "../components/modals/m-add-session/m-add-session.component";
 import {NbDialogService} from "@nebular/theme";
+import {END_POINTS} from "../../../providers/utils";
+import {GeneralService} from "../../../providers";
+import {AppService} from "../../../core";
 
 @Component({
   selector: 'app-films-home',
@@ -13,13 +16,20 @@ export class FilmsHomeComponent implements OnInit {
 
   loading:boolean = false;
   formHeader: any = FormGroup;
+  user: any;
+  items: any = [];
   constructor(private fb: FormBuilder,
-              private dialogService: NbDialogService) { }
+              private dialogService: NbDialogService,
+              private generalService: GeneralService,
+              private userService: AppService,) { }
 
   ngOnInit(): void {
     this.fieldReactive();
+    this.getCoursesList()
+    console.log()
 
   }
+
   get rolSemestre() {
     const sesion: any = sessionStorage.getItem('rolSemesterLeng');
     if (sesion) {
@@ -36,7 +46,17 @@ export class FilmsHomeComponent implements OnInit {
     this.formHeader = this.fb.group(controls);
   }
 
-
+getCoursesList(){
+    this.loading = true
+    const serviceName = 'select-course';
+    const params:any = {
+      semester_id: this.rolSemestre.semestre.id,
+    };
+    this.generalService.nameIdParams$(serviceName,this.userService.user.person.id, params).subscribe(res => {
+        this.items = res.data || [];
+      },() => {this.loading = false}, ()=>{this.loading = false}
+    )
+}
   addSession(){
     this.dialogService.open(MAddSessionComponent, {
       dialogClass: 'dialog-limited-height',
