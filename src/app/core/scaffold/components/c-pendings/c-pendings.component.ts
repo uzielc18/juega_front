6561101278e7fbc847b04bc90017f288ec936205
Ever Saul <ever.hilasaca@cloudcomputing.com.pe>
2022-10-05 12:46:59 -0500

    05/10/2022 12:46:57 -> cambios

--- src/app/core/scaffold/components/c-pendings/c-pendings.component.ts
+++ src/app/core/scaffold/components/c-pendings/c-pendings.component.ts
@@ -12,43 +12,9 @@ import {Router} from "@angular/router";
 export class CPendingsComponent implements OnInit {
 
   data: any = [];
-  daysLeft: any;
-  hoursLeft: any;
-  minutesLeft: any;
-  secondsLeft: any;
-  expiredDays: any;
-  expiredHours: any;
-  expiredMinutes: any;
-  expiredSeconds: any;
-  daysMap = {
-    '=0': '',
-    '=1': '1 dia,',
-    other: '# dias,',
-  };
-
-  hoursMap = {
-    '=0': '',
-    '=1': '1 hora,',
-    other: '# horas,',
-  };
-
-  minutesMap = {
-    '=0': '',
-    '=1': '1 minuto.',
-    other: '# minutos.',
-  };
   constructor(private generalService: GeneralService,
               private appService: AppService,
-              private router: Router,) {
-
-      setInterval(() => {
-        this.data.map((m: any) => {
-          m.newCount = m.fecha_fin
-          this.getTimes(m.newCount)
-        })
-      }, 1000)
-
-  }
+              private router: Router,) {}
 
   ngOnInit(): void {
     this.getPendings();
@@ -72,27 +38,7 @@ export class CPendingsComponent implements OnInit {
       console.log(res)
     })
   }
-  getTimes(fechaFin: any){
-    const countDate = new Date(fechaFin?.replace(' ', 'T')).getTime();
-    const now = new Date().getTime();
-    const left = countDate - now;
-    const expired = now - countDate;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    this.daysLeft = Math.floor(left / day);
-    this.hoursLeft = Math.floor((left % day) / hour);
-    this.minutesLeft = Math.floor((left % hour) / minute);
-    this.secondsLeft = Math.floor((left % minute) / second);
-
-    this.expiredDays = Math.floor(expired / day);
-    this.expiredHours = Math.floor((expired % day) / hour);
-    this.expiredMinutes = Math.floor((expired % hour) / minute);
-    this.expiredSeconds = Math.floor((expired % minute) / second);
-  }
   locationElement(item: any){
     this.router.navigate([`/pages/asignatures/course/${item.id_carga_curso_docente}/element/${item.element_id}`]);
   }
