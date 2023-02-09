a8f2786a832d3247cc41f948766751b650e97199
ever.hilasaca <everhilasaca@upeu.edu.pe>
2023-02-09 17:51:11 -0500

    09/02/2023 17:51:08 -> cambios

--- src/app/pages/manage/lamb-sync-canva/components/modals/m-enrollment-canva/m-enrollment-canva.component.ts
+++ src/app/pages/manage/lamb-sync-canva/components/modals/m-enrollment-canva/m-enrollment-canva.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {GeneralService} from "../../../../../../providers";
-import {NbDialogRef} from "@nebular/theme";
+import {NbDialogRef, NbToastrService} from "@nebular/theme";
+import {FormControl} from "@angular/forms";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-m-enrollment-canva',
@@ -9,11 +11,100 @@ import {NbDialogRef} from "@nebular/theme";
 })
 export class MEnrollmentCanvaComponent implements OnInit {
 
+  loading: boolean = false;
+  closeAndGetInfo: boolean = false;
+  termino: any = new FormControl('');
+
+  listEnrllments: any = [];
+  contadores: any;
+  selectedItem = 20;
+  pagination: any = {
+    page: 1,
+    per_page: 20,
+    sizePage: 0,
+    sizeListData: 0,
+    isDisabledPage: false,
+  };
+  pagesCount: any[] = [20, 30, 50, 100];
   @Input() formHeader: any;
   constructor(private generalService: GeneralService,
-              public activeModal: NbDialogRef<MEnrollmentCanvaComponent>,) { }
+              public activeModal: NbDialogRef<MEnrollmentCanvaComponent>,
+              private toastrService: NbToastrService,) { }
 
   ngOnInit(): void {
+    this.getListEnrollments();
+  }
+
+  getListEnrollments () {
+    const serviceName = 'list-enrollments-programa';
+    const forms = this.formHeader;
+    const params = {
+      semester_id: forms.semestre,
+      q:  this.termino.value || '',
+    }
+    this.loading = true;
+    this.generalService.nameIdParams$(serviceName, forms.programa_estudio?.id, params).subscribe(res => {
+      if(res.success){
+        this.listEnrllments = res.data.matriculas;
+        this.contadores = res.data.contadores;
+      }
+
+    }, () => {this.loading = false}, ()=> {this.loading = false})
+  }
+
+  closeModal() {
+    this.activeModal.close('');
+  }
+
+  loadPage($event: any): any {
+    this.pagination.page = $event;
+    this.getListEnrollments();
+
+  }
+  sizeTable($event: any): any {
+    this.pagination.per_page = $event;
+    this.getListEnrollments();
+  }
+
+  syncCanvasEnrrollemnt() {
+    const serviceName = 'canva-insert-enrollment';
+    const forms = this.formHeader;
+    const params = {
+      semester_id: forms.semestre,
+    }
+    Swal.fire({
+      title: 'Sincronizar',
+      text: '¿ Esta seguro de sincronizar con canva ? ',
+      backdrop: true,
+      icon: 'question',
+      // animation: true,
+      showCloseButton: true,
+      showCancelButton: true,
+      showConfirmButton: true,
+      confirmButtonColor: '#00244E',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+      // timer: 2000,
+    }).then((result:any) => {
+      if (result.isConfirmed) {
+        this.loading = true;
+        this.generalService.nameIdParams$(serviceName, forms.programa_estudio?.id, params).subscribe(res => {
+          if(res.success){
+            this.toastrService.info(status, `${res.message}`);
+            this.getListEnrollments();
+          }
+        }, () => {this.loading= false}, () => {this.loading = false})
+      }
+    });
+  }
+  limpiarBusqueda() {
+    this.closeAndGetInfo = false;
+    this.termino.setValue('');
+    this.getListEnrollments();
+  }
+  searchEnrollement() {
+    this.closeAndGetInfo = true;
+    this.getListEnrollments();
   }
 
 }
