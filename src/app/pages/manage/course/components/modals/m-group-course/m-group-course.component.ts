62b7f9af2db92a408734e84f7a17c561989f1be4
ever.hilasaca <everhilasaca@upeu.edu.pe>
2023-02-22 15:14:11 -0500

    22/02/2023 15:14:08 -> cambios

--- src/app/pages/manage/course/components/modals/m-group-course/m-group-course.component.ts
+++ src/app/pages/manage/course/components/modals/m-group-course/m-group-course.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NbDialogRef} from "@nebular/theme";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {GeneralService} from "../../../../../../providers";
 
 @Component({
   selector: 'app-m-group-course',
@@ -30,16 +31,14 @@ export class MGroupCourseComponent implements OnInit {
     array_A: [],
     array_B: [],
   }
-  constructor(public activeModal: NbDialogRef<MGroupCourseComponent>, private fb: FormBuilder) { }
+  constructor(public activeModal: NbDialogRef<MGroupCourseComponent>, private fb: FormBuilder, private generalService: GeneralService) { }
 
   ngOnInit(): void {
   }
 
   private fieldGrupManual() {
     const controls = {
-      nombre_grupo: ['', [Validators.required]],
-      opc: ['NEWS'],
-      id_group: [''],
+      nombre_curso: ['', [Validators.required]],
     };
     this.formGroupManual = this.fb.group(controls);
   }
@@ -120,5 +119,68 @@ export class MGroupCourseComponent implements OnInit {
   }
   getListMember() {
 
+  }
+  saveGM() {
+    const serviceName = 'courses';
+    const item = this.item;
+    const forms =  this.formGroupManual.value;
+    const params = {
+      course_id: item.id,
+      programa_estudio_id: item.programa_estudio_id,
+      area_id: item.area_id,
+      semester_id: item.semester_id,
+      sede_area_id: item.sede_area_id,
+      persons_teacher_id: item.persons_teacher_id,
+      person_id_canva: item.person_id_canva,
+      courses_type_id:  item.courses_type_id,
+      courses_type_nombre: item.courses_type_nombre,
+      id_carga_curso: item.id_carga_curso,
+      id_carga_curso_docente: null,
+      nombre: forms.nombre_curso,
+      course_nombre: forms.nombre_curso,
+      descripcion: item.descripcion,
+      portada: item.portada,
+      portada_miniatura: item.portada_miniatura,
+      silabo: item.silabo,
+      guia_curso: item.guia_curso,
+      grupo: item.grupo,
+      ciclo: item.ciclo,
+      aula: item.aula,
+      idioma: item.idioma,
+      cupos: item.cupos,
+      precio: item.precio,
+      precio2: item.precio2,
+      clave_inscripcion: item.clave_inscripcion,
+      porcentaje_aprobacion: item.porcentaje_aprobacion,
+      min_nota: item.min_nota,
+      tipo_nota: item.tipo_nota,
+      fecha_inicio: item.fecha_inicio,
+      fecha_fin: item.fecha_fin,
+      fecha_inicio_inscripcion: item.fecha_inicio_inscripcion,
+      fecha_fin_inscripcion: item.fecha_fin_inscripcion,
+      chat: item.chat,
+      consultas: item.consultas,
+      inscripcion: item.inscripcion,
+      pago: item.pago,
+      estado: item.estado,
+      ht: item.ht,
+      hp: item.hp,
+      hnp: item.hnp,
+      modo_curso: item.modo_curso,
+      url_video: item.url_video,
+      url_compartir: item.url_compartir,
+      descuento: item.descuento,
+      clave: item.clave,
+      code_category: item.code_category,
+      join_url: item.join_url,
+      host_key: item.host_key,
+      id_zoom_meetings: item.id_zoom_meetings,
+    }
+    this.loading = true;
+    this.generalService.addNameData$(serviceName, params).subscribe(res => {
+      if(res.success) {
+
+      }
+    }, () => {this.loading = false}, () => {this.loading = false})
   }
 }
